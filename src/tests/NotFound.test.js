94a1ebbc7b8621b10e50602b6371b18e0c1b71dd
Lúcio BJ <lbjunq@hotmail.com>
2021-08-18 17:53:05 -0300

    Adicionado o quarto requisito

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Tests About page', () => {
+  it('redirects to NotFound if unknown path is typed', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pagina/que-nao-existe/');
+    const noMatchTitle = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/,
+    });
+    expect(noMatchTitle).toBeInTheDocument();
+    const noMatchGif = screen.getByAltText(/Pikachu crying because the page/);
+    expect(noMatchGif).toBeInTheDocument();
+    expect(noMatchGif.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+
+    // Jeito alternativo levemente pior
+    // const noMatchGif = screen.getAllByRole('img');
+    // expect(noMatchGif[1]).toBeInTheDocument();
+    // expect(noMatchGif[1].src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
