93d645f86e61aace1c33e800d26f04981082dde1
Lúcio BJ <lbjunq@hotmail.com>
2021-08-18 14:47:07 -0300

    Commit Inicial

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+describe ('Tests the App links', () => {
+  it('has three links', () => {});
+  it('has a Home link', () => {});
+  it('has a About link', () => {});
+  it('has a Favorite Pokemons link', () => {});
+})
+
+describe ('Tests the App routes', () => {
+  it('redirects to "/" after clicking on Home link', () => {});
+  it('redirects to "/about" after clicking on About link', () => {});
+  it('redirects to "/favorites" after clicking on Favorite Pokemons link', () => {});
+})
